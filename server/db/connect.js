9e5086649bba3b7823db5a62286ed4701fac28d7
Amir Ali anwar <amiralianwar611@gmail.com>
2023-04-02 15:59:16 +0500

    working on the server server side setup

---
+++ server/db/connect.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose'
+
+const connectDB = (url) => {
+  try {
+      mongoose.set("strictQuery", false);
+      mongoose.connect(url, () => {
+      console.log("MongoDB connected");
+    });
+
+  } catch (error) {
+    console.log(error.message);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
