0b3247dc5951d2a0a74f86e88b54e9838aabd024
Amir Ali anwar <amiralianwar611@gmail.com>
2023-04-02 22:04:42 +0500

    added models for database schema

---
+++ server/model/Message.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+const MessageSchema =new mongoose.Schema({
+  conversationId: {
+    type: String,
+    required: true,
+  },
+  userId: {
+    type: String,
+    required: true,
+  },
+  desc: {
+    type: String,
+    required: true,
+  },
+},{
+  timestamps:true
+});
+
+const Message = mongoose.model("Message", MessageSchema)
+export default  Message
