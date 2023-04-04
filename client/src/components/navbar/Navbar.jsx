2368d7c1500f12fc441c68c23874c4ca4a1d1299
Amir Ali anwar <amiralianwar611@gmail.com>
2023-04-04 21:20:03 +0500

    added TrustedBy, Navbar components

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
     <div className={`${active ? "navbar active" : "navbar"}`}>
       <div className="container">
         <div className="logo">
-          <span className="text">fiverr</span>
+          <span className="text">fiver</span>
           <span className="dot">.</span>
         </div>
         <div className="links">
