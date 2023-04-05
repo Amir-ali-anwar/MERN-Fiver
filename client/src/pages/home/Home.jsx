8abc258ef6432c0dc823991a0694a232771555c5
Amir Ali anwar <amiralianwar611@gmail.com>
2023-04-05 21:11:56 +0500

    working on the homepage

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -4,6 +4,7 @@ import TrustedBy from '../../components/trustedBy/TrustedBy'
 import Slide from '../../components/slide/Slide'
 import CartCard from '../../components/catcard/cartCard'
 import { cards, projects } from "../../data";
+import "./Home.scss";
 const Home = () => {
   return (
     <div className="home">
