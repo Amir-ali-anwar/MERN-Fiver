57be7b0b5219188a3cd0d4a597181ee4c193fed2
Amir Ali anwar <amiralianwar611@gmail.com>
2023-04-10 20:19:49 +0500

    UI completed

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,64 +1,122 @@
 import React, { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 import "./Navbar.scss";
-const Navbar = () => {
-  const [active, setActive] = useState(false)
+
+function Navbar() {
+  const [active, setActive] = useState(false);
   const [open, setOpen] = useState(false);
 
+  const { pathname } = useLocation();
+
   const isActive = () => {
-    window.scrollY > 0 ? setActive(true) : setActive(fasle);
-  }
+    window.scrollY > 0 ? setActive(true) : setActive(false);
+  };
+
   useEffect(() => {
-    window.addEventListener('scroll', isActive)
+    window.addEventListener("scroll", isActive);
     return () => {
-      window.removeEventListener('scroll', isActive)
-    }
-  }, [])
+      window.removeEventListener("scroll", isActive);
+    };
+  }, []);
+
+  // const currentUser = null
+
   const currentUser = {
     id: 1,
-    username: 'Amir Ali Anwar',
-    isSeller: true
-  }
+    username: "Anna",
+    isSeller: true,
+  };
+
   return (
-    <div className={`${active ? "navbar active" : "navbar"}`}>
+    <div className={active || pathname !== "/" ? "navbar active" : "navbar"}>
       <div className="container">
         <div className="logo">
-          <span className="text">fiver</span>
+          <Link className="link" to="/">
+            <span className="text">liverr</span>
+          </Link>
           <span className="dot">.</span>
         </div>
         <div className="links">
-          <span>Fiverr Business</span>
+          <span>Liverr Business</span>
           <span>Explore</span>
           <span>English</span>
-          <span>Sign in </span>
-          {!currentUser?.isSeller && <span>Become a Seller </span>}
-          {!currentUser && <button>Join</button>}
-          {
-           currentUser && (
+          {!currentUser?.isSeller && <span>Become a Seller</span>}
+          {currentUser ? (
             <div className="user" onClick={()=>setOpen(!open)}>
-            <img
+              <img
                 src="https://images.pexels.com/photos/1115697/pexels-photo-1115697.jpeg?auto=compress&cs=tinysrgb&w=1600"
                 alt=""
               />
               <span>{currentUser?.username}</span>
-             {open && <div className="options">
-                {
-                 currentUser?.isSeller  && (
+              {open && <div className="options">
+                {currentUser.isSeller && (
                   <>
-                  <span>Gigs</span>
-                  <span>Add</span>
+                    <Link className="link" to="/mygigs">
+                      Gigs
+                    </Link>
+                    <Link className="link" to="/add">
+                      Add New Gig
+                    </Link>
                   </>
-                 ) }
-                 <span>Orders</span>
-                 <span>Messages</span>
-                 <span>Logout</span>
+                )}
+                <Link className="link" to="/orders">
+                  Orders
+                </Link>
+                <Link className="link" to="/messages">
+                  Messages
+                </Link>
+                <Link className="link" to="/">
+                  Logout
+                </Link>
               </div>}
-             </div> 
-           ) 
-          }
+            </div>
+          ) : (
+            <>
+              <span>Sign in</span>
+              <Link className="link" to="/register">
+                <button>Join</button>
+              </Link>
+            </>
+          )}
         </div>
       </div>
+      {(active || pathname !== "/") && (
+        <>
+          <hr />
+          <div className="menu">
+            <Link className="link menuLink" to="/">
+              Graphics & Design
+            </Link>
+            <Link className="link menuLink" to="/">
+              Video & Animation
+            </Link>
+            <Link className="link menuLink" to="/">
+              Writing & Translation
+            </Link>
+            <Link className="link menuLink" to="/">
+              AI Services
+            </Link>
+            <Link className="link menuLink" to="/">
+              Digital Marketing
+            </Link>
+            <Link className="link menuLink" to="/">
+              Music & Audio
+            </Link>
+            <Link className="link menuLink" to="/">
+              Programming & Tech
+            </Link>
+            <Link className="link menuLink" to="/">
+              Business
+            </Link>
+            <Link className="link menuLink" to="/">
+              Lifestyle
+            </Link>
+          </div>
+          <hr />
+        </>
+      )}
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,20 +1,21 @@
-import React from 'react'
-import Featured from '../../components/featured/Featured'
-import TrustedBy from '../../components/trustedBy/TrustedBy'
-import Slide from '../../components/slide/Slide'
-import CartCard from '../../components/catcard/cartCard'
-import { cards, projects } from "../../data";
+import React from "react";
 import "./Home.scss";
-const Home = () => {
+import Featured from "../../components/featured/Featured";
+import TrustedBy from "../../components/trustedBy/TrustedBy";
+import Slide from "../../components/slide/Slide";
+import CatCard from "../../components/catCard/CatCard";
+import ProjectCard from "../../components/projectCard/ProjectCard";
+import { cards, projects } from "../../data";
+
+function Home() {
   return (
     <div className="home">
       <Featured />
       <TrustedBy />
       <Slide slidesToShow={5} arrowsScroll={5}>
-        {cards?.map((card) => (
-            <CartCard key={card.id} card={card} />
-          ))
-        }
+        {cards.map((card) => (
+          <CatCard key={card.id} card={card} />
+        ))}
       </Slide>
       <div className="features">
         <div className="container">
@@ -183,8 +184,13 @@ const Home = () => {
           </div>
         </div>
       </div>
+      <Slide slidesToShow={4} arrowsScroll={4}>
+        {projects.map((card) => (
+          <ProjectCard key={card.id} card={card} />
+        ))}
+      </Slide>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
