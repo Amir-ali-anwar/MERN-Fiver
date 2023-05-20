91a09ca0f1ab34ba9619b028ea71d7f61f768284
Amir Ali anwar <amiralianwar611@gmail.com>
2023-05-20 12:02:06 +0500

    added routes configuration

--- server/app.js
+++ server/app.js
@@ -9,10 +9,18 @@ import connectDB from './db/connect.js'
 import "express-async-errors";
 // importing Routes
 import authRoutes from './routes/authRoutes.js'
+import conversationRoutes from './routes/conversationRoutes.js'
+import gitRoutes from './routes/gigRoutes.js'
+import messageRoutes from './routes/messageRoute.js'
+import orderRoutes from './routes/orderRoute.js'
+import reviewRoutes from './routes/reviewRoute.js'
+import userRoutes from './routes/userRoute.js'
 dotenv.config();
 
 const app = express();
+app.use(cors())
 app.use(express.json());
+app.use(cookieParser())
 app.disable('x-powered-by')
 if (process.env.NODE_ENV !== "production") {
     app.use(morgan("tiny"));
@@ -24,6 +32,13 @@ app.get("/", (req, res) => {
 // routes
 
 app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/conversations", conversationRoutes)
+app.use("/api/v1/gigs", gitRoutes)
+app.use("/api/v1/messages", messageRoutes)
+app.use("/api/v1/orders", orderRoutes)
+app.use("/api/v1/reviews", reviewRoutes)
+app.use("/api/v1/users", userRoutes)
+
 // middlewares
 app.use(notFoundMiddleware);
 app.use(errorHandler)
