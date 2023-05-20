1cda7f4e3aa039b7968e32dc6315329302bf6fb9
Amir Ali anwar <amiralianwar611@gmail.com>
2023-05-20 11:38:36 +0500

    Fixed CastError

--- server/middleware/error-handler.js
+++ server/middleware/error-handler.js
@@ -14,10 +14,11 @@ const errorHandler = (err, req, res, next) => {
     customError.msg = Object.values(err.errors).map((item) => item.message.join(',')),
     customError.statusCode = 400
   }
+  if (err.name === 'CastError') {
+    CustomError.msg = `No item found with the ${err.value}`
+    CustomError.statusCode = 400
+  }
   return res.status(customError.statusCode).json({ msg: customError.msg });
-
-  return res.status(500).json({ msg: err });
-  // return res.status(500).json({ msg: err });
 };
 export default errorHandler;
 
@@ -26,7 +27,7 @@ export default errorHandler;
 
 
 // There are three types of mongoose errors
-// 1) Validation errors , 2) Duplicate Error, 3) Cast Error
+// 1) Validation errors , 2) Duplicate Error, 3) Cast Error(syntax error like mongoose id or something another)
 
 // there are three  types of errors that we are handling in this file, first one is the of our customAPI error(errors that we are throughing in the
 // from our files) and the second
