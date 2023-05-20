9aaad33ab162a9e58bfaa3a50b0a57a611277edf
Amir Ali anwar <amiralianwar611@gmail.com>
2023-05-20 11:35:00 +0500

    adding eror handling

--- server/middleware/error-handler.js
+++ server/middleware/error-handler.js
@@ -1,24 +1,34 @@
-import CustomAPIError from '../error/CustomAPIError.js';
-import StatusCodes from 'http-status-codes'
+import CustomAPIError from "../error/CustomAPIError.js";
+import StatusCodes from "http-status-codes";
 const errorHandler = (err, req, res, next) => {
-  const customError={
-    statusCode:err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
-    msg:err.message || "Something went wrong"
+  const customError = {
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || "Something went wrong",
+  };
+  if (err.code && err.code === 11000) {
+    customError.msg = `Duplicate value entered for the ${Object.keys(
+      err.keyValue
+    )} Please entered anoter value`;
   }
- if(err instanceof CustomAPIError) {
-  return res.send(err.statusCode).json({msg:err.message})
- }
- if(err.code && err.code ===11000){
-  customError.msg = `Duplicate value entered for the ${Object.keys(err.keyValue)} Please entered anoter value`
- }
- return res.status(customError.statusCode).json({msg:customError.msg})
-  
+  if (err.name === 'ValidationError') {
+    customError.msg = Object.values(err.errors).map((item) => item.message.join(',')),
+    customError.statusCode = 400
+  }
+  return res.status(customError.statusCode).json({ msg: customError.msg });
+
   return res.status(500).json({ msg: err });
   // return res.status(500).json({ msg: err });
 };
 export default errorHandler;
 
-// there are three  types of errors that we are handling in this file, first one is the of our customAPI error(errors that we are throughing in the 
+
+
+
+
+// There are three types of mongoose errors
+// 1) Validation errors , 2) Duplicate Error, 3) Cast Error
+
+// there are three  types of errors that we are handling in this file, first one is the of our customAPI error(errors that we are throughing in the
 // from our files) and the second
 
 //one is the default error
